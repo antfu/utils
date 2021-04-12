683056a098f0eb42023d83eebc3a68dc65bec34f
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-12 20:55:58 +0800

    feat: new functions

--- src/array.ts
+++ src/array.ts
@@ -29,9 +29,19 @@ export function mergeArrayable<T>(...args: Nullable<Arrayable<T>>[]): Array<T> {
  *
  * @example const [odd, even] = partition([1, 2, 3, 4], i => i % 2 != 0)
  */
-export function partition<T>(array: T[], filter: (i: T, idx: number, arr: T[]) => any) {
+export function partition<T>(array: readonly T[], filter: (i: T, idx: number, arr: readonly T[]) => any) {
   const pass: T[] = []
   const fail: T[] = []
   array.forEach((e, idx, arr) => (filter(e, idx, arr) ? pass : fail).push(e))
   return [pass, fail]
+}
+
+export function uniq<T>(array: readonly T[]): T[] {
+  return Array.from(new Set(array))
+}
+
+export function last<T>(array: readonly T[]): T | undefined {
+  if (!array.length)
+    return undefined
+  return array[array.length - 1]
 }
