76ed6055de4474454199ffd8a8cb97fd447ae285
Enzo Innocenzi <enzo@innocenzi.dev>
2021-06-09 15:46:29 +0200

    chore: improve tap's jsdoc

--- src/function.ts
+++ src/function.ts
@@ -15,7 +15,14 @@ export function invoke(fn: Fn) {
 }
 
 /**
- * Pass the value through the callback, and return the former.
+ * Pass the value through the callback, and return the value.
+ *
+ * @example
+ * function createUser(name: string): User {
+ *   return tap(new User, user => {
+ *     user.name = name
+ *   })
+ * }
  */
 export function tap<T>(value: T, callback: (value: T) => void): T {
   callback(value)
