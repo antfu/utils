8250877636368b94354019708181f679346b2340
Enzo Innocenzi <enzo@innocenzi.dev>
2021-06-09 15:32:59 +0200

    feat(function): tap utility

--- src/function.ts
+++ src/function.ts
@@ -13,3 +13,11 @@ export function batchInvoke(functions: Nullable<Fn>[]) {
 export function invoke(fn: Fn) {
   return fn()
 }
+
+/**
+ * Pass the value through the callback, and return the former.
+ */
+export function tap<T>(value: T, callback: (value: T) => void): T {
+  callback(value)
+  return value
+}
