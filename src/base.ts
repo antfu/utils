973ac7422b780b7929d5b7b3d76fa7af324ed6ed
Johann Schopplich <mail@johannschopplich.com>
2021-06-18 13:44:57 +0200

    feat(base): assertion function type guard (#2)

--- src/base.ts
+++ src/base.ts
@@ -1,3 +1,5 @@
-export const assert = (condition: boolean, ...infos: any[]) => { if (!condition) throw new Error(...infos) }
+export const assert = (condition: boolean, message: string): asserts condition => {
+  if (!condition) throw new Error(message)
+}
 export const toString = Object.prototype.toString
 export const noop = () => {}
