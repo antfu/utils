cbbf7b8887dc1806f36e9e72dc18260bc53be0a5
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-25 11:37:50 +0800

    feat: createPromiseLock

--- src/promise.ts
+++ src/promise.ts
@@ -1,3 +1,4 @@
+import { remove } from './array'
 import { Fn } from './types'
 
 export interface SingletonPromiseReturn<T> {
@@ -49,4 +50,45 @@ export function sleep(ms: number, callback?: Fn<any>) {
       resolve()
     }, ms),
   )
+}
+
+/**
+ * Create a promise lock
+ *
+ * @example
+ * ```
+ * const lock = createPromiseLock()
+ *
+ * lock.run(async () => {
+ *   await doSomething()
+ * })
+ *
+ * // in anther context:
+ * await lock.wait() // it will wait all tasking finished
+ * ```
+ */
+export function createPromiseLock() {
+  const locks: Promise<any>[] = []
+
+  return {
+    async run<T = void>(fn: () => Promise<T>): Promise<T> {
+      const p = fn()
+      locks.push(p)
+      try {
+        return await p
+      }
+      finally {
+        remove(locks, p)
+      }
+    },
+    async wait(): Promise<void> {
+      await Promise.allSettled(locks)
+    },
+    isWaiting() {
+      return Boolean(locks.length)
+    },
+    clear() {
+      locks.length = 0
+    },
+  }
 }
