72068541928d3a77300efd05f39511253fe9dbe8
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-25 11:50:22 +0800

    feat(array): partition support multiple filters

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,4 +1,4 @@
-import { flattenArrayable, range } from './array'
+import { flattenArrayable, partition, range } from './array'
 
 it('flattenArrayable', () => {
   expect(flattenArrayable()).toEqual([])
@@ -14,4 +14,45 @@ it('range', () => {
   expect(range(2)).toEqual([0, 1])
   expect(range(2, 5)).toEqual([2, 3, 4])
   expect(range(2, 10, 2)).toEqual([2, 4, 6, 8])
+})
+
+it('partition', () => {
+  const data = range(10)
+
+  expect(
+    partition(data, i => i % 2),
+  ).toEqual([
+    [1, 3, 5, 7, 9],
+    [0, 2, 4, 6, 8],
+  ])
+
+  expect(
+    partition(
+      data,
+      i => i % 3 === 0,
+      i => i % 2 === 0,
+    ),
+  ).toEqual([
+    [0, 3, 6, 9],
+    [2, 4, 8],
+    [1, 5, 7],
+  ])
+
+  expect(
+    partition(
+      data,
+      i => i,
+    ),
+  ).toHaveLength(2)
+
+  expect(
+    partition(
+      data,
+      i => i,
+      i => i,
+      i => i,
+      i => i,
+      i => i,
+    ),
+  ).toHaveLength(6)
 })
--- src/array.ts
+++ src/array.ts
@@ -31,17 +31,35 @@ export function mergeArrayable<T>(...args: Nullable<Arrayable<T>>[]): Array<T> {
   return args.flatMap(i => toArray(i))
 }
 
+export type PartitionFilter<T> = (i: T, idx: number, arr: readonly T[]) => any
+
 /**
  * Divide an array into two parts by a filter function
  *
  * @category Array
  * @example const [odd, even] = partition([1, 2, 3, 4], i => i % 2 != 0)
  */
-export function partition<T>(array: readonly T[], filter: (i: T, idx: number, arr: readonly T[]) => any) {
-  const pass: T[] = []
-  const fail: T[] = []
-  array.forEach((e, idx, arr) => (filter(e, idx, arr) ? pass : fail).push(e))
-  return [pass, fail]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>): [T[], T[]]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>, f2: PartitionFilter<T>): [T[], T[], T[]]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>, f2: PartitionFilter<T>, f3: PartitionFilter<T>): [T[], T[], T[], T[]]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>, f2: PartitionFilter<T>, f3: PartitionFilter<T>, f4: PartitionFilter<T>): [T[], T[], T[], T[], T[]]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>, f2: PartitionFilter<T>, f3: PartitionFilter<T>, f4: PartitionFilter<T>, f5: PartitionFilter<T>): [T[], T[], T[], T[], T[], T[]]
+export function partition<T>(array: readonly T[], f1: PartitionFilter<T>, f2: PartitionFilter<T>, f3: PartitionFilter<T>, f4: PartitionFilter<T>, f5: PartitionFilter<T>, f6: PartitionFilter<T>): [T[], T[], T[], T[], T[], T[], T[]]
+export function partition<T>(array: readonly T[], ...filters: PartitionFilter<T>[]): any {
+  const result: T[][] = new Array(filters.length + 1).fill(null).map(() => [])
+
+  array.forEach((e, idx, arr) => {
+    let i = 0
+    for (const filter of filters) {
+      if (filter(e, idx, arr)) {
+        result[i].push(e)
+        return
+      }
+      i += 1
+    }
+    result[i].push(e)
+  })
+  return result
 }
 
 /**
