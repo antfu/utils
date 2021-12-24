46ae25d11b30da1c90918c8e935e666a22f3acad
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-12-25 02:45:53 +0800

    feat!: do not dep on dom in typescript lib (#7)

--- src/is.ts
+++ src/is.ts
@@ -1,8 +1,12 @@
+import { toString } from './base'
+
+// @ts-ignore
 export const isBrowser = typeof window !== 'undefined'
 export const isDef = <T = any>(val?: T): val is T => typeof val !== 'undefined'
 export const isBoolean = (val: any): val is boolean => typeof val === 'boolean'
 export const isFunction = <T extends Function> (val: any): val is T => typeof val === 'function'
 export const isNumber = (val: any): val is number => typeof val === 'number'
 export const isString = (val: unknown): val is string => typeof val === 'string'
-export const isObject = (val: any): val is object => toString.call(val) === '[object Object]'
-export const isWindow = (val: any): val is Window => typeof window !== 'undefined' && toString.call(val) === '[object Window]'
+export const isObject = (val: any): val is object => toString(val) === '[object Object]'
+// @ts-ignore
+export const isWindow = (val: any): boolean => typeof window !== 'undefined' && toString(val) === '[object Window]'
