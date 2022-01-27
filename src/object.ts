258833b8d7538ad246e5fe4fc34a36c1c39a849e
Lucifer <zhu273151892@sina.com>
2022-01-28 04:31:30 +0800

    fixed(object): Fix the problem that the return value is always empty when omitUndefined = true (#9)

--- src/object.ts
+++ src/object.ts
@@ -113,7 +113,7 @@ function isMergableObject(item: any): item is Object {
 export function objectPick<O, T extends keyof O>(obj: O, keys: T[], omitUndefined = false) {
   return keys.reduce((n, k) => {
     if (k in obj)
-      if (!omitUndefined || !obj[k] === undefined) n[k] = obj[k]
+      if (!omitUndefined || obj[k] !== undefined) n[k] = obj[k]
     return n
   }, {} as Pick<O, T>)
 }
