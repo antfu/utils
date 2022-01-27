729efeae9ec32e1264f1a274ab05f665970f89e2
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-27 13:20:53 +0800

    feat: inline `@antfu/p`

---
+++ src/p.test.ts
@@ -0,0 +1,74 @@
+import { describe, expect, it } from 'vitest'
+import { p as P } from './p'
+
+const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+describe('should', () => {
+  it('p', async() => {
+    const p = P()
+    let dummy = 0
+    p.add((async() => {
+      await timeout(100)
+      dummy += 1
+      return 4
+    })())
+    expect(dummy).toBe(0)
+    await p
+    expect(dummy).toBe(1)
+  })
+
+  it('chain array map', async() => {
+    expect(
+      await P([1, 2, 3, 4, 5])
+        .map(async(i) => {
+          await timeout(10)
+          return i * i
+        })
+        .filter(i => i > 10)
+        .reduce((a, b) => a + b, 0),
+    )
+      .toEqual(41)
+  })
+
+  it('concurrency: 1', async() => {
+    let running = 0
+
+    const promises = Array.from({ length: 100 }, async(_, i: number) => {
+      running++
+      expect(running).to.be.lessThanOrEqual(1)
+      running--
+      return i
+    })
+
+    const results = await P(promises, { concurrency: 1 }).reduce((a, b) => a + b, 0)
+    expect(results).to.be.equal(4950)
+  })
+
+  it('concurrency: 4', async() => {
+    let running = 0
+
+    const promises = Array.from({ length: 100 }, async() => {
+      running++
+      expect(running).to.be.lessThanOrEqual(4)
+      running--
+    })
+
+    await P(promises, { concurrency: 4 })
+  })
+
+  it('fails with wrong format', async() => {
+    try {
+      await P([], { concurrency: 1.5 })
+    }
+    catch (e) {
+      expect(e).toBeInstanceOf(TypeError)
+    }
+
+    try {
+      await P([], { concurrency: 0 })
+    }
+    catch (e) {
+      expect(e).toBeInstanceOf(TypeError)
+    }
+  })
+})
---
+++ src/p.ts
@@ -0,0 +1,101 @@
+import pLimit from 'p-limit'
+
+/**
+ * Internal marker for filtered items
+ */
+const VOID = Symbol('p-void')
+
+interface POptions {
+  /**
+   * How many promises are resolved at the same time.
+   */
+  concurrency?: number | undefined
+}
+
+class PInstance<T = any> extends Promise<Awaited<T>[]> {
+  private promises = new Set<T | Promise<T>>()
+
+  get promise(): Promise<Awaited<T>[]> {
+    let batch
+    const items = [...Array.from(this.items), ...Array.from(this.promises)]
+
+    if (this.options?.concurrency) {
+      const limit = pLimit(this.options.concurrency)
+      batch = Promise.all(items.map(p => limit(() => p)))
+    }
+    else {
+      batch = Promise.all(items)
+    }
+
+    return batch.then(l => l.filter((i: any) => i !== VOID))
+  }
+
+  constructor(public items: Iterable<T> = [], public options?: POptions) {
+    super(() => {})
+  }
+
+  add(...args: (T | Promise<T>)[]) {
+    args.forEach((i) => {
+      this.promises.add(i)
+    })
+  }
+
+  map<U>(fn: (value: Awaited<T>, index: number) => U): PInstance<Promise<U>> {
+    return new PInstance(
+      Array.from(this.items)
+        .map(async(i, idx) => {
+          const v = await i
+          if ((v as any) === VOID)
+            return VOID as unknown as U
+          return fn(v, idx)
+        }),
+      this.options,
+    )
+  }
+
+  filter(fn: (value: Awaited<T>, index: number) => boolean | Promise<boolean>): PInstance<Promise<T>> {
+    return new PInstance(
+      Array.from(this.items)
+        .map(async(i, idx) => {
+          const v = await i
+          const r = await fn(v, idx)
+          if (!r)
+            return VOID as unknown as T
+          return v
+        }),
+      this.options,
+    )
+  }
+
+  forEach(fn: (value: Awaited<T>, index: number) => void): Promise<void> {
+    return this.map(fn).then()
+  }
+
+  reduce<U>(fn: (previousValue: U, currentValue: Awaited<T>, currentIndex: number, array: Awaited<T>[]) => U, initialValue: U): Promise<U> {
+    return this.promise.then(array => array.reduce(fn, initialValue))
+  }
+
+  clear() {
+    this.promises.clear()
+  }
+
+  then(fn?: () => PromiseLike<any>) {
+    const p = this.promise
+    if (fn)
+      return p.then(fn)
+    else
+      return p
+  }
+
+  catch(fn?: (err: unknown) => PromiseLike<any>) {
+    return this.promise.catch(fn)
+  }
+
+  finally(fn?: () => void) {
+    return this.promise.finally(fn)
+  }
+}
+
+export function p<T = any>(items?: Iterable<T>, options?: POptions): PInstance<T> {
+  return new PInstance(items, options)
+}
