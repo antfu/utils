8a07c616fa468a035473db4ed68de38f231bf896
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-11 04:22:08 +0800

    feat(promise): new `createControlledPromise` method

--- src/promise.ts
+++ src/promise.ts
@@ -88,3 +88,36 @@ export function createPromiseLock() {
     },
   }
 }
+
+/**
+ * Promise with `resolve` and `reject` methods of itself
+ */
+export interface ControlledPromise<T = void> extends Promise<T> {
+  resolve(value: T | PromiseLike<T>): void
+  reject(reason?: any): void
+}
+
+/**
+ * Return a Promise with `resolve` and `reject` methods
+ *
+ * @category Promise
+ * @example
+ * ```
+ * const promise = createControlledPromise()
+ *
+ * await promise
+ *
+ * // in anther context:
+ * promise.resolve(data)
+ * ```
+ */
+export function createControlledPromise<T>(): ControlledPromise<T> {
+  let resolve: any, reject: any
+  const promise = new Promise<T>((_resolve, _reject) => {
+    resolve = _resolve
+    reject = _reject
+  }) as ControlledPromise<T>
+  promise.resolve = resolve
+  promise.reject = reject
+  return promise
+}
