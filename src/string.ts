af5e12d57137c3d33b771b3ec3dd9108d81020bb
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-10 19:50:34 +0800

    feat: random str

--- src/string.ts
+++ src/string.ts
@@ -50,3 +50,19 @@ export function template(str: string, ...args: any[]): string {
     return args[index]
   })
 }
+
+// port from nanoid
+// https://github.com/ai/nanoid
+const urlAlphabet = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
+/**
+ * Generate a random string
+ * @category String
+ */
+export function randomStr(size = 16, dict = urlAlphabet) {
+  let id = ''
+  let i = size
+  const len = dict.length
+  while (i--)
+    id += dict[(Math.random() * len) | 0]
+  return id
+}
