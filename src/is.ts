f8698cd427d5cbfec25ef771e435527b8ccaf01c
子瞻 Luci <haru.lucinyan@gmail.com>
2022-11-30 07:55:10 +0800

    feat(is): add some type guard (#25)

--- src/is.ts
+++ src/is.ts
@@ -6,6 +6,10 @@ export const isFunction = <T extends Function> (val: any): val is T => typeof va
 export const isNumber = (val: any): val is number => typeof val === 'number'
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isObject = (val: any): val is object => toString(val) === '[object Object]'
+export const isUndefined = (val: any): val is undefined => toString(val) === '[object Undefined]'
+export const isNull = (val: any): val is null => toString(val) === '[object Null]'
+export const isRegExp = (val: any): val is RegExp => toString(val) === '[object RegExp]'
+export const isDate = (val: any): val is Date => toString(val) === '[object Date]'
 
 // @ts-ignore
 export const isWindow = (val: any): boolean => typeof window !== 'undefined' && toString(val) === '[object Window]'
