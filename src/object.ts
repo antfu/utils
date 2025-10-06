12f3cb29da446f1880d7a4fb4f9551d29b8094a3
Trung Dang <dangquoctrung123@gmail.com>
2025-10-06 08:48:23 +0700

    feat(object): new `objectOmit` function (#51)

--- src/object.ts
+++ src/object.ts
@@ -191,6 +191,17 @@ export function objectPick<O extends object, T extends keyof O>(obj: O, keys: T[
 }
 
 /**
+ * Create a new subset object by omit giving keys
+ *
+ * @category Object
+ */
+export function objectOmit<O extends object, T extends keyof O>(obj: O, keys: T[], omitUndefined = false) {
+  return Object.fromEntries(Object.entries(obj).filter(([key, value]) => {
+    return (!omitUndefined || value !== undefined) && !keys.includes(key as T)
+  })) as Omit<O, T>
+}
+
+/**
  * Clear undefined fields from an object. It mutates the object
  *
  * @category Object
